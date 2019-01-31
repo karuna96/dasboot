6c83dec524f71a477d45229805eebe6b9bd67454
CHERUPALLY <karuna.cherupally@capgemini.com>
2019-01-31 11:42:15 +0530

    logs

--- src/main/java/com/cg/controller/HomeController.java
+++ src/main/java/com/cg/controller/HomeController.java
@@ -1,13 +1,19 @@
 package com.cg.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+	public static final Logger logger =LogManager.getRootLogger();
+	
 	@RequestMapping("/")
 	public String home() {
+		logger.info("Helloo!!");
+		
 		return "Hello Capgeminite!";
 	}
 }
